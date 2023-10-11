ef8eb9c065b64ac1d39396753edea6f5e1631604
haeshka <106382537+DeveloperHaeshka@users.noreply.github.com>
2023-10-11 20:53:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var urlParams = new URLSearchParams(window.location.search);
 let tg = window.Telegram.WebApp;
 let userid = urlParams.get('userid')
 let balance = parseInt(urlParams.get('balance'))
+let StartBalance = balance
 let result = document.getElementById('result')
 let finish = document.getElementById('finish')
 tg.expand()
@@ -28,30 +29,117 @@ play.addEventListener('click', () => {
     if (summ > 0) {
         rx = GetRandomInt(101)
         let StatusSumm = ''
-        if (rx >= 0 && rx <= 10) {
-            x = 2
-            StatusSumm = '+'
+
+        if (balance < 100000) {
+            if (rx >= 0 && rx <= 10) {
+                x = 2
+                StatusSumm = '+'
+            }
+            if (rx >= 11 && rx <= 25) {
+                x = 0.5
+                StatusSumm = '-'
+            }
+            if (rx >= 26 && rx <= 50) {
+                x = 1.7
+                StatusSumm = '+'
+            }
+            if (rx >= 51 && rx <= 75) {
+                x = 0.7
+                StatusSumm = '-'
+            }
+            if (rx >= 76 && rx <= 90) {
+                x = 0.3
+                StatusSumm = '-'
+            }
+            if (rx >= 91 && rx <= 100) {
+                x = 0
+                StatusSumm = '-'
+            }
+    
         }
-        if (rx >= 11 && rx <= 25) {
-            x = 0.5
-            StatusSumm = '-'
+        if (balance > 100000) {
+            if (rx >= 0 && rx <= 10) {
+                x = 1.3
+                StatusSumm = '+'
+            }
+            if (rx >= 11 && rx <= 25) {
+                x = 0.5
+                StatusSumm = '-'
+            }
+            if (rx >= 26 && rx <= 50) {
+                x = 1.7
+                StatusSumm = '+'
+            }
+            if (rx >= 51 && rx <= 75) {
+                x = 0.7
+                StatusSumm = '-'
+            }
+            if (rx >= 76 && rx <= 90) {
+                x = 0.3
+                StatusSumm = '-'
+            }
+            if (rx >= 91 && rx <= 100) {
+                x = 0
+                StatusSumm = '-'
+            }
+
         }
-        if (rx >= 26 && rx <= 50) {
-            x = 1.7
-            StatusSumm = '+'
+        let PolBalance = parseInt(balance / 2)
+        if (summ > PolBalance) {
+            if (rx >= 0 && rx <= 10) {
+                x = 1.3
+                StatusSumm = '+'
+            }
+            if (rx >= 11 && rx <= 25) {
+                x = 0.5
+                StatusSumm = '-'
+            }
+            if (rx >= 26 && rx <= 50) {
+                x = 0.7
+                StatusSumm = '-'
+            }
+            if (rx >= 51 && rx <= 75) {
+                x = 0.7
+                StatusSumm = '-'
+            }
+            if (rx >= 76 && rx <= 90) {
+                x = 0.3
+                StatusSumm = '-'
+            }
+            if (rx >= 91 && rx <= 100) {
+                x = 0
+                StatusSumm = '-'
+            }
         }
-        if (rx >= 51 && rx <= 75) {
-            x = 0.7
-            StatusSumm = '-'
+
+        if (balance + 25000 > StartBalance) {
+            if (rx >= 0 && rx <= 10) {
+                x = 1.3
+                StatusSumm = '+'
+            }
+            if (rx >= 11 && rx <= 25) {
+                x = 0.5
+                StatusSumm = '-'
+            }
+            if (rx >= 26 && rx <= 50) {
+                x = 1.7
+                StatusSumm = '+'
+            }
+            if (rx >= 51 && rx <= 75) {
+                x = 0.7
+                StatusSumm = '-'
+            }
+            if (rx >= 76 && rx <= 90) {
+                x = 0.3
+                StatusSumm = '-'
+            }
+            if (rx >= 91 && rx <= 100) {
+                x = 0
+                StatusSumm = '-'
+            }
+    
         }
-        if (rx >= 76 && rx <= 90) {
-            x = 0.3
-            StatusSumm = '-'
-        }
-        if (rx >= 91 && rx <= 100) {
-            x = 0
-            StatusSumm = '-'
-        }
+
         let StavkaWinish = summ * x
         if (x == 0) {
             StavkaWinish = summ
